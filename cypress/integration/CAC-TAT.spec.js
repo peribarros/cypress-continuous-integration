cbe750be03115c6cfd1547d5c57cfffecaa19a66
Peri Barros <peribarros@gmail.com>
2024-01-10 08:18:44 -0300

    Update formulario

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,9 +8,26 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    // beforeEach - antes de cada teste
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
 
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Peri')
+        cy.get('#lastName').type('Barros')
+        cy.get('#email').type('peribarros@gmail.com')
+        cy.get('#open-text-area').type('Texto')
+        // classe button e a propriedade type submit 
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+
+    })
+
+
   })
