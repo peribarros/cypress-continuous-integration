c04404d769d901a513cdeeffeb89da33d900fe48
Peri Barros <peribarros@gmail.com>
2024-01-09 22:48:31 -0300

    Update package.json

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,6 +11,6 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
 
-        cy.title().should('be.qual', ('Central de Atendimento ao Cliente TAT'))
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
   })
