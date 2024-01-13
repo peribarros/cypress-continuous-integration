2dbffd9a3530d1601a25990cbbd88dd36fd494a9
Peri Barros <peribarros@gmail.com>
2024-01-12 22:05:26 -0300

    Export video module

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,17 @@ module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      { "reporter"; "mochawesome", 
+        "reporterOptions"; 
+          { "reportDir"; "cypress/report/mochawesome-report", 
+           "overwrite"; false, 
+           "html"; true, 
+           "json"; false, 
+           "timestamp"; "mmddyyyy_HHMMss" }}
+    },
   },
+  video: true
 })
+
+
